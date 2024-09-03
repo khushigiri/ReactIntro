b82b017e683eeb4c62346aa67a15c26415dca493
khushigiri <02khushi.giri@gmail.com>
2024-09-03 22:02:28 +0530

    State and Props

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import ClassComponent from "./components/ClassComponents";
 import FunctionComponent from "./components/FunctionalComponents";
+import { useState } from "react";
 
 function App() {
+  const author = "rk";
+
+  const [name, setName] = useState("DT");
   return (
     <div className="App">
       <p>Class Components</p>
@@ -10,7 +14,13 @@ function App() {
       <br />
 
       <p>Functional Component</p>
-      <FunctionComponent />
+      {/* <FunctionComponent name="DevTown" age={20} author={author} /> */}
+      <FunctionComponent
+        name={name}
+        age={20}
+        author={author}
+        setName={setName}
+      />
     </div>
   );
 }
--- first-react-app/src/components/FunctionalComponents.jsx
+++ first-react-app/src/components/FunctionalComponents.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FunctionComponent = () => {
+const FunctionComponent = (props) => {
+  const [count, setCount] = useState(0);
+  const [changeName, setChangeName] = useState("");
+  const reduceCount = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
       <p>This is Functional Component</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me to increment/add by 1
+      </button>
+      <br />
+      <br />
+      <button onClick={reduceCount}>Click here to decrement</button>
+      <h2>{count}</h2>
+      <h4>
+        My Company is {props.name}, it is of {props.age} years old and the
+        author is {props.author}.
+      </h4>
+      <input onChange={(e) => setChangeName(e.target.value)} />
+      <button onClick={() => props.setName(changeName)}>Change name</button>
     </div>
   );
 };
